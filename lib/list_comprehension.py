26c506d4f92e4d298356ecd9e9932b02312c092c
Cole Perry <colecperry@gmail.com>
2023-04-04 10:34:31 -0700

    Done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_nums = [n for n in num_list if n % 2 == 0]
+    return even_nums
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation_strings = [(n + "!") for n in sentence_list]
+    return exclamation_strings
+                           
